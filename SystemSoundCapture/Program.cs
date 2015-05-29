a49106905e7c1f0a59f0f576406e71ad948891b0
DeltaCoding <deltadrizz@gmail.com>
2015-05-29 15:04:16 +0200

    Initial Commit

---
+++ SystemSoundCapture/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+using CSCore;
+using CSCore.SoundIn;
+using CSCore.Codecs.WAV;
+
+namespace SystemSoundCapture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            startRec();
+            listen();
+        }
+
+        private static void startRec()
+        {
+            string filename = "./" + DateTime.Now.ToString("d_MMM_yyyy_HH_mm_ssff") + ".wav";
+            using (WasapiCapture capture = new WasapiLoopbackCapture())
+            {
+                capture.Initialize();
+
+                using (WaveWriter w = new WaveWriter(filename, capture.WaveFormat))
+                {
+                    capture.DataAvailable += (s, e) =>
+                    {
+                        w.Write(e.Data, e.Offset, e.ByteCount);
+                    };
+
+                    Console.ReadKey();
+                    capture.Start();
+                    Console.ReadKey();
+                    capture.Stop();
+                }
+            }
+        }
+
+        private static void listen()
+        {
+
+        }
+    }
+}
